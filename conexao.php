55026afecfac174cdb529edf2e7cccd530db0c9e
Lucas Mathews <111380007+LucasMathewws@users.noreply.github.com>
2026-03-18 11:38:30 -0300

    Refactor conexao.php for database connection
    Removed unnecessary include statements and added database connection constants.

--- conexao.php
+++ conexao.php
@@ -1,6 +1,8 @@
 <?php
-include();
-include();
-include();
-include();
+define('HOST', 'localhost');
+define('USUARIO', 'root');
+define('SENHA','');
+define('DB', 'crud');
+
+$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Erro de Processamento');
 ?>
